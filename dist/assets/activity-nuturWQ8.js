6561df396bf1233ef227c46a8aa37af862e56ca9
albarakahmcsl <albarakahmcsl25@gmail.com>
2025-09-03 13:55:49 +0400

    Updated index.html

--- dist/assets/activity-nuturWQ8.js
+++ dist/assets/activity-nuturWQ8.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-DA8XNciY.js";/**
+import{c as t}from"./index-BTOutWqw.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
