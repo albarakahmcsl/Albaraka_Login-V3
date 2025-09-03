fa43fb28f44f2c18b28e7ad80025f60bdcc666f1
albarakahmcsl <albarakahmcsl25@gmail.com>
2025-09-03 14:15:37 +0400

    Publish application

--- dist/assets/activity-CEhhdhLv.js
+++ dist/assets/activity-CEhhdhLv.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-BTOutWqw.js";/**
+import{c as t}from"./index-BEPwOk0G.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
